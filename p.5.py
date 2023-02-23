a5eb5083cf700103869aef5cb7ace1d8d79c88f2
DmytroGFMF <126173544+DmytroGFMF@users.noreply.github.com>
2023-02-23 17:25:09 +0200

    Add files via upload

---
+++ p.5.py
@@ -0,0 +1,10 @@
+import random
+list1 = [random.randint(1, 20) for i in range(5)]
+list2 = [random.randint(1, 20) for i in range(5)]
+list3 = []
+list3 = list1 + list2
+print(list1)
+print(list2)
+print(list3)
+
+
